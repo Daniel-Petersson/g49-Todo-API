da3f65b121e42b0f36757adecaf35dd70941f4b0
binleiden <daniel.petersson83@gmail.com>
2024-06-11 12:00:50 +0200

    added @Repository annotation

--- src/main/java/Repository/RoleRepository.java
+++ src/main/java/Repository/RoleRepository.java
@@ -1,10 +1,13 @@
 package Repository;
 
-import entity.Role;
+import domain.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
+    //select * from role where name = ?
     Optional<Role> findByName(String name);
 }
