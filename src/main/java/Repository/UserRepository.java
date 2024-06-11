e47c8b08b326ce8c0234f93bc48baafc4ebf27ba
binleiden <daniel.petersson83@gmail.com>
2024-06-11 11:59:50 +0200

    added @repository annotation

--- src/main/java/Repository/UserRepository.java
+++ src/main/java/Repository/UserRepository.java
@@ -1,14 +1,16 @@
 package Repository;
 
-import entity.User;
+import domain.entity.User;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
-
+    //Check if the email exists in the database
     Boolean existsByEmail(String email);
 
     @Transactional
