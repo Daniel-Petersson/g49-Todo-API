b6abc0ed293fe5cbbc3d4ab4423c6d72242c3a1d
binleiden <daniel.petersson83@gmail.com>
2024-06-11 10:36:04 +0200

    Added transactional annotation

--- src/main/java/Repository/UserRepository.java
+++ src/main/java/Repository/UserRepository.java
@@ -1,6 +1,7 @@
 package Repository;
 
 import entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,10 +11,12 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     Boolean existsByEmail(String email);
 
+    @Transactional
     @Modifying
     @Query("UPDATE User u SET u.expired = :status WHERE u.email = :email")
     void updateExpiredByEmail(@Param("email")String email, @Param("status")boolean status);
 
+    @Transactional
     @Modifying
     @Query("UPDATE User u SET u.password = :password WHERE u.email = :email")
     void updatePasswordByEmail(@Param("email") String email, @Param("password")String newPassword);
