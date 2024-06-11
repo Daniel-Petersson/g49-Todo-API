5dd18c01d4c816ec15cfc1d09d1b89fa91c98da3
binleiden <daniel.petersson83@gmail.com>
2024-06-11 11:58:09 +0200

    created dto for user and role

---
+++ src/main/java/domain/dto/RoleDTOForm.java
@@ -0,0 +1,8 @@
+package domain.dto;
+
+public class RoleDTOForm {
+    // todo : add validation annotations if needed
+
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/domain/dto/RoleDTOView.java
@@ -0,0 +1,13 @@
+package domain.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoleDTOView {
+    private Long id;
+    private String name;
+}
