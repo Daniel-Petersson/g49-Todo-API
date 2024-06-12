616eb064c3d1bb4380afeac0897c5046bd9769ec
binleiden <daniel.petersson83@gmail.com>
2024-06-12 16:07:22 +0200

    Finished todo for TaskRepository

--- src/main/java/se/lexicon/g49todoapi/Repository/TaskRepository.java
+++ src/main/java/se/lexicon/g49todoapi/Repository/TaskRepository.java
@@ -1,17 +1,30 @@
 package se.lexicon.g49todoapi.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import se.lexicon.g49todoapi.domain.entity.Task;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    //todo: select tasks contain title
-    // select tasks by person's id
-    // select tasks by status
-    // select tasks by date between start and end
-    // select all unassigned tasks
-    // select all unfinished tasks
-    // select all unfinished and overdue tasks
 
+    // select tasks contain title
+    List<Task> findByTitleContainingIgnoreCase(String title);
+    // select tasks by person's id
+    List<Task> findByPersonId(Long id);
+    // select tasks by status
+    List<Task> findTaskByDone(boolean done);
+    // select tasks by date between start and end
+    List<Task> findByDeadlineBetween(LocalDate start, LocalDate end);
+    // select all unassigned tasks
+    List<Task> findByPersonIsNull();
+    // select all unfinished tasks
+    @Query("SELECT t FROM Task t WHERE t.done = false")
+    List<Task> selectUnFinishedTasks();
+    // select all unfinished and overdue tasks
+    @Query("SELECT t FROM Task t WHERE t.done = false AND current_date > t.deadline")
+    List<Task> selectUnFinishedAndOverdueTasks();
 }
