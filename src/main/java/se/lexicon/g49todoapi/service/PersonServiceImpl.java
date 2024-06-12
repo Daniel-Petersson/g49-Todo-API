2139b26539fdf51e63af44aa2bfd084dd6bc1af9
binleiden <daniel.petersson83@gmail.com>
2024-06-12 12:36:43 +0200

    Created service classes and added todo

---
+++ src/main/java/se/lexicon/g49todoapi/service/PersonServiceImpl.java
@@ -0,0 +1,5 @@
+package se.lexicon.g49todoapi.service;
+
+public class PersonServiceImpl {
+    //todo: Implement methods
+}
---
+++ src/main/java/se/lexicon/g49todoapi/service/TaskServiceImpl.java
@@ -0,0 +1,5 @@
+package se.lexicon.g49todoapi.service;
+
+public class TaskServiceImpl {
+    //todo: Implement methods
+}
