88444244f45b374d740fe5b40a3e6c9995d96aaf
binleiden <daniel.petersson83@gmail.com>
2024-06-20 10:44:34 +0200

    Refactored TaskServiceImpl for improved task and person DTO conversion

--- src/main/java/se/lexicon/g49todoapi/service/TaskServiceImpl.java
+++ src/main/java/se/lexicon/g49todoapi/service/TaskServiceImpl.java
@@ -1,39 +1,46 @@
 package se.lexicon.g49todoapi.service;
 
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.lexicon.g49todoapi.Repository.PersonRepository;
 import se.lexicon.g49todoapi.Repository.TaskRepository;
+import se.lexicon.g49todoapi.domain.dto.PersonDTOView;
 import se.lexicon.g49todoapi.domain.dto.TaskDTOForm;
 import se.lexicon.g49todoapi.domain.dto.TaskDTOView;
+import se.lexicon.g49todoapi.domain.dto.UserDTOView;
+import se.lexicon.g49todoapi.domain.entity.Person;
 import se.lexicon.g49todoapi.domain.entity.Task;
+import se.lexicon.g49todoapi.exeption.DataNotFoundException;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+@AllArgsConstructor
 @Service
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
+    private final PersonRepository personRepository;
 
-    @Autowired
-    public TaskServiceImpl(TaskRepository taskRepository) {
-        this.taskRepository = taskRepository;
-    }
 
     @Override
     @Transactional
     public TaskDTOView create(TaskDTOForm taskDTOForm) {
         if (taskDTOForm == null) throw new IllegalArgumentException("Form cannot be empty");
 
-
+        if (taskDTOForm.getAssignedPerson() == null) {
+            throw new IllegalArgumentException("Assigned person cannot be null");
+        }
+        Person person = personRepository.findById(taskDTOForm.getAssignedPerson()).orElseThrow(()->new DataNotFoundException("some error"));
         Task task = Task.builder()
                 .title(taskDTOForm.getTitle())
                 .description(taskDTOForm.getDescription())
                 .deadline(taskDTOForm.getDeadline())
                 .done(taskDTOForm.isDone())
+                .person(person)
                 .build();
         Task savedTask = taskRepository.save(task);
 
@@ -46,19 +53,22 @@ public class TaskServiceImpl implements TaskService {
 
         return convertToDTOView(task);
     }
-
+    //Todo
     @Override
     @Transactional
     public TaskDTOView update(TaskDTOForm taskDTOForm) {
-        if (taskDTOForm == null) throw new IllegalArgumentException("Form cannot be empty");
+        /*if (taskDTOForm == null) throw new IllegalArgumentException("Form cannot be empty");
         Task existingTask = taskRepository.findById(taskDTOForm.getId())
                 .orElseThrow(() -> new IllegalArgumentException("Task with the id: " + taskDTOForm.getId() + " not found"));
+        Person person = personRepository.findById(taskDTOForm.getPerson().getId()).orElseThrow(()-> new IllegalArgumentException("Person with the id: "+ taskDTOForm.getPerson().getId() + " not found"));
         existingTask.setTitle(taskDTOForm.getTitle());
         existingTask.setDescription(taskDTOForm.getDescription());
         existingTask.setDeadline(taskDTOForm.getDeadline());
         existingTask.setDone(taskDTOForm.isDone());
+        existingTask.setPerson(person);
         Task updatedTask = taskRepository.save(existingTask);
-        return convertToDTOView(updatedTask);
+        return convertToDTOView(updatedTask);*/
+        return null;
     }
 
     @Override
@@ -120,6 +130,13 @@ public class TaskServiceImpl implements TaskService {
                 .description(task.getDescription())
                 .deadline(task.getDeadline())
                 .done(task.isDone())
+                .person(PersonDTOView.builder()
+                        .id(task.getId())
+                        .name(task.getPerson().getName())
+                        .user(UserDTOView.builder()
+                                .email(task.getPerson().getUser().getEmail())
+                                .build())
+                        .build())
                 .build();
     }
 }
